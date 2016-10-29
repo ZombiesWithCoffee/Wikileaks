6d300fae7e9f22e947ce99febd847737330a3242
defiancedev <defiancedev.@gmail.com>
2016-10-29 16:33:37 -0700

    Added bulk processing, app settings, filters, caching.

---
+++ WikiLeaks/Managers/AppManager.cs
@@ -0,0 +1,111 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WikiLeaks.Extensions;
+using WikiLeaks.Models;
+
+namespace WikiLeaks.Managers
+{
+    public class AppManager
+    {
+        public AppSetting Settings { get; set; }
+
+        private MainWindow _mainWindow = null;
+
+        public AppManager(MainWindow wnd, string pathToFile)
+        {
+            _mainWindow = wnd;
+
+            Settings = new AppSetting();
+
+            if (string.IsNullOrWhiteSpace(pathToFile) || !File.Exists(pathToFile))
+            {
+                pathToFile = Path.Combine(EnvironmentEx.AppDataFolder, "default.appsettings.json");
+
+                if (!File.Exists(pathToFile)&& CreateDefaultFile(pathToFile) ==false)
+                {
+                    _mainWindow.UpdateUi("FAILED TO GET APP SETTINGS FILE!", "messagebox.show");
+                    return;
+                }
+            }
+
+            try
+            {
+                string tmp = File.ReadAllText(pathToFile);
+                if (string.IsNullOrWhiteSpace(tmp))
+                {
+                    CreateDefaultFile(pathToFile);
+                    tmp = File.ReadAllText(pathToFile);
+                }
+                Settings = JsonConvert.DeserializeObject<AppSetting>(tmp);
+            }
+            catch(Exception ex)
+            {
+                Debug.Assert(false, ex.Message);
+                _mainWindow.UpdateUi(ex.Message, "messagebox.show");
+            }
+
+        }
+
+
+        protected bool CreateDefaultFile(string pathToFile)
+        {
+            try
+            {
+                using (FileStream fs = File.Create(pathToFile))
+                {
+                    Settings.SearchStartId = 1;
+                    Settings.SearchEndId = 20;
+                    Settings.BaseUrl = "https://wikileaks.org/podesta-emails/emaildid/";
+                    Settings.CacheFolder = Path.Combine( EnvironmentEx.AppDataFolder, "Cache\\");
+
+                    Settings.FilterFolder = Path.Combine(EnvironmentEx.AppDataFolder, "Filters\\");
+
+                    string dataasstring = JsonConvert.SerializeObject(Settings);
+                    byte[] info = new UTF8Encoding(true).GetBytes(dataasstring);
+                    fs.Write(info, 0, info.Length);
+                    byte[] data = new byte[] { 0x0 };
+                    fs.Write(data, 0, data.Length);
+                }
+            }
+            catch(Exception ex)
+            {
+                Debug.Assert(false, ex.Message);
+                _mainWindow.UpdateUi(ex.Message, "messagebox.show");
+                return false;
+            }
+            return true;
+        }
+
+        public bool SaveSettings(string pathToFile)
+        {
+            if(string.IsNullOrWhiteSpace(pathToFile))
+                pathToFile = Path.Combine(EnvironmentEx.AppDataFolder, "default.appsettings.json");
+            
+            try
+            {
+                using (FileStream fs = File.Create(pathToFile))
+                {
+                    string dataasstring = JsonConvert.SerializeObject(Settings);
+                    byte[] info = new UTF8Encoding(true).GetBytes(dataasstring);
+                    fs.Write(info, 0, info.Length);
+                    byte[] data = new byte[] { 0x0 };
+                    fs.Write(data, 0, data.Length);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.Assert(false, ex.Message);
+                _mainWindow.UpdateUi(ex.Message, "messagebox.show");
+                return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ WikiLeaks/Models/Document.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WikiLeaks.Models
+{
+    public class Document
+    {
+        public int LeakId { get; set; }
+
+        public string LeakUrl { get; set; }
+
+        public string Body { get; set; }
+
+        public bool IsAttachment { get; set; }
+
+        public string Type { get; set; }
+
+        public DateTime DocDate { get; set; }
+    }
+}
